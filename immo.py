7934a0e0450f8b017eaf6864ca2072adf93febfa
VHoa <nguyenviethoa95@gmail.com>
2019-01-16 14:32:20 +0100

    test connection and upload file to s3

--- immo.py
+++ immo.py
@@ -1,33 +1,31 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-import re
-import pandas as pd
-from tabulate import tabulate
-import os
-import lxml
-import googleapiclient
+import boto3
 import requests
-from pathlib import  Path
-import urllib.request
+import config
+
 # launch url
 url = "https://www.nuernberg.de/internet/pr/amtsblatt_2018.html"
 
+client = boto3.client('s3')
+
 # create a new Firefox session
-binary =r'C:\Program Files\chromedriver.exe'
-driver = webdriver.Chrome(binary)
+driver = webdriver.Chrome()
 driver.implicitly_wait(30)
 driver.get(url)
-#r = requests.get("")
+
 #get the list of the download attribute
 links_list = driver.find_elements_by_partial_link_text("Amtsblatt der Stadt")
-i =0
+i = 0
 for link in links_list:
     filename= link.text+'.pdf'
     # request for the pdf file
-    url =  link.get_attribute('href')
-    r= requests.get(url)
-    with open('test'+'_'+str(i)+'.pdf', 'wb') as fd:
-         fd.write(r.content)
+    url = link.get_attribute('href')
+    r = requests.get(url)
+    session = boto3.Session(region_name=config.CURRENT_REGION,
+                            aws_access_key_id=config.AWS_CONFIG['AWS_SERVER_PUBLIC_KEY'],
+                            aws_secret_access_key=config.AWS_CONFIG['AWS_SERVER_SECRET_KEY'])
+    client = session.resource('s3')
+    key = 'text1.txt'
+    res = client.Bucket('amtsblatt').put_object(Key=filename, Body=r.content)
     i= i+1
 driver.close()
